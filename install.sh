1622246c795031fd8f2570709e8d2cb16d29f595
oppressing <oppressings@gmail.com>
2017-11-10 13:04:23 +0000

    Create install.sh
    to automate the process more if you want to install it on a bunch of PIs(that you own of course ) you can do the following
    
    curl (url) -o (output) ; sh (output name)
    wget (url) ; sh (file has to be a .sh)
    
    I prefer the first command i had some issues with the second one

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Credit to @Oppressings on twitter
+#Auto setup for a rapssbery pi miner useing the cpuminer-multi
+#Any issues please contact me there
+#dependencies
+
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev -y 
+sudo apt-get update -y
+sudo apt-get install git -y
+sudo apt-get install -y --force-yes autoconf automake libtool
+sudo apt-get install screen -y
+sudo apt-get install build-essential -y
+
+#Installing the miner
+
+git clone https://github.com/tpruvot/cpuminer-multi
+cd cpuminer-multi
+
+#Building the miner
+
+./autogen.sh
+./build.sh
+
+#Mining
+#
+
+screen ./cpuminer -a cryptonight -o stratum+tcp://teracycle.net:3333 -p x -u (wallet)  --api-bind 0
